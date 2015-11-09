1d1b461a800d2c73389cbddca5be330065ffafa5
Felipe Luiz Tortella <felipetortella@gmail.com>
2015-11-09 17:34:19 -0200

    Primeiro commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] groups;
+
+	// Use this for initialization
+	void Start () {
+		spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void spawnNext(){
+		int i = Random.Range (0, groups.Length);
+		//coloca um bloco na posiçao do elemento
+		Instantiate (groups [i], transform.position, Quaternion.identity);
+		//Debug.Log ("Instanciou");
+	}
+}
