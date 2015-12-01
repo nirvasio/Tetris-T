401ea09bd135f3256853878d8f54720918da5938
Felipe Luiz Tortella <felipetortella@gmail.com>
2015-12-01 21:39:36 -0200

    Tentativa um

--- Assets/Group.cs
+++ Assets/Group.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Group : MonoBehaviour {
 
 	float lastFall;
+	
+	public Prefab explosionPrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -15,10 +17,188 @@ public class Group : MonoBehaviour {
 	}
 	
 	void Update() {
+		
+		// Move TELA Right
+		if (Input.GetKeyDown(KeyCode.D)) {
+			
+			if(Grid.lado == 1){
+			// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid1[x, y] != null)
+							if (Grid.grid1[x, y].parent == transform)
+								Grid.grid1[x, y] = null;
+						
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+						if(Grid.grid2[(int)v.x, (int)v.y] != null){
+							// Instantiate Explosion
+							Instantiate(explosionPrefab, transform.position, transform.rotation);							
+							// Destroy the gameobject
+							Destroy (gameObject);
+							break;
+						}
+						Grid.grid2[(int)v.x, (int)v.y] = child;
+				}
+			}
+			
+			if(Grid.lado == 2){
+			// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid2[x, y] != null)
+							if (Grid.grid2[x, y].parent == transform)
+								Grid.grid2[x, y] = null;
+						
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);	
+						if(Grid.grid3[(int)v.x, (int)v.y] != null){
+							// Instantiate Explosion
+							Instantiate(explosionPrefab, transform.position, transform.rotation);							
+							// Destroy the gameobject
+							Destroy (gameObject);
+							break;
+						}
+						Grid.grid3[(int)v.x, (int)v.y] = child;
+				}
+			}
+			
+			if(Grid.lado == 3){
+			// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid2[x, y] != null)
+							if (Grid.grid3[x, y].parent == transform)
+								Grid.grid3[x, y] = null;
+						
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+						if(Grid.grid4[(int)v.x, (int)v.y] != null){
+							// Instantiate Explosion
+							Instantiate(explosionPrefab, transform.position, transform.rotation);							
+							// Destroy the gameobject
+							Destroy (gameObject);
+							break;
+						}					
+						Grid.grid4[(int)v.x, (int)v.y] = child;
+				}
+			}
+			
+			if(Grid.lado == 4){
+			// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid2[x, y] != null)
+							if (Grid.grid4[x, y].parent == transform)
+								Grid.grid4[x, y] = null;
+						
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+						if(Grid.grid1[(int)v.x, (int)v.y] != null){
+							// Instantiate Explosion
+							Instantiate(explosionPrefab, transform.position, transform.rotation);							
+							// Destroy the gameobject
+							Destroy (gameObject);
+							break;
+						}					
+						Grid.grid1[(int)v.x, (int)v.y] = child;
+				}
+			}
+		}
+		
+		// Move TELA Left
+		if (Input.GetKeyDown(KeyCode.A)) {
+			
+			if(Grid.lado == 1){
+			// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid1[x, y] != null)
+							if (Grid.grid1[x, y].parent == transform)
+								Grid.grid1[x, y] = null;
+						
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);	
+						if(Grid.grid4[(int)v.x, (int)v.y] != null){
+							// Instantiate Explosion
+							Instantiate(explosionPrefab, transform.position, transform.rotation);							
+							// Destroy the gameobject
+							Destroy (gameObject);
+							break;
+						}
+						Grid.grid4[(int)v.x, (int)v.y] = child;
+				}
+			}
+			
+			if(Grid.lado == 2){
+			// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid2[x, y] != null)
+							if (Grid.grid2[x, y].parent == transform)
+								Grid.grid2[x, y] = null;
+						
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+						if(Grid.grid1[(int)v.x, (int)v.y] != null){
+							// Instantiate Explosion
+							Instantiate(explosionPrefab, transform.position, transform.rotation);							
+							// Destroy the gameobject
+							Destroy (gameObject);
+							break;
+						}
+						Grid.grid1[(int)v.x, (int)v.y] = child;
+				}
+			}
+			
+			if(Grid.lado == 3){
+			// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid2[x, y] != null)
+							if (Grid.grid3[x, y].parent == transform)
+								Grid.grid3[x, y] = null;
+						
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+						if(Grid.grid2[(int)v.x, (int)v.y] != null){
+							// Instantiate Explosion
+							Instantiate(explosionPrefab, transform.position, transform.rotation);							
+							// Destroy the gameobject
+							Destroy (gameObject);
+							break;
+						}
+						Grid.grid2[(int)v.x, (int)v.y] = child;
+				}
+			}
+			
+			if(Grid.lado == 4){
+			// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid2[x, y] != null)
+							if (Grid.grid4[x, y].parent == transform)
+								Grid.grid4[x, y] = null;
+						
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+						if(Grid.grid3[(int)v.x, (int)v.y] != null){
+							// Instantiate Explosion
+							Instantiate(explosionPrefab, transform.position, transform.rotation);							
+							// Destroy the gameobject
+							Destroy (gameObject);
+							break;
+						}
+						Grid.grid3[(int)v.x, (int)v.y] = child;
+				}
+			}
+			
+		}
+		
 		// Move Left
 		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
 			// Modify position
-			transform.position += new Vector3(-1, 0, 0);
+			transform.position += new Vector3(-1, transform.position.y, transform.position.z);
 			
 			// See if valid
 			if (isValidGridPos())
@@ -26,13 +206,13 @@ public class Group : MonoBehaviour {
 				updateGrid();
 			else
 				// It's not valid. revert.
-				transform.position += new Vector3(1, 0, 0);
+				transform.position += new Vector3(1, transform.position.y, transform.position.z);
 		}
 		
 		// Move Right
 		else if (Input.GetKeyDown(KeyCode.RightArrow)) {
 			// Modify position
-			transform.position += new Vector3(1, 0, 0);
+			transform.position += new Vector3(1, transform.position.y, transform.position.z);
 			
 			// See if valid
 			if (isValidGridPos())
@@ -40,12 +220,12 @@ public class Group : MonoBehaviour {
 				updateGrid();
 			else
 				// It's not valid. revert.
-				transform.position += new Vector3(-1, 0, 0);
+				transform.position += new Vector3(-1, transform.position.y, transform.position.z);
 		}
 		
 		// Rotate
 		else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			transform.Rotate(0, 0, -90);
+			transform.Rotate(transform.rotation.x, transform.rotation.y, -90);
 			
 			// See if valid
 			if (isValidGridPos())
@@ -53,14 +233,14 @@ public class Group : MonoBehaviour {
 				updateGrid();
 			else
 				// It's not valid. revert.
-				transform.Rotate(0, 0, 90);
+				transform.Rotate(transform.rotation.x, transform.rotation.y, 90);
 		}
 		
 		// Move Downwards and Fall
 		else if (Input.GetKeyDown(KeyCode.DownArrow) ||
 		         Time.time - lastFall >= 1) {
 			// Modify position
-			transform.position += new Vector3(0, -1, 0);
+			transform.position += new Vector3(transform.position.x, -1, transform.position.z);
 			
 			// See if valid
 			if (isValidGridPos()) {
@@ -68,7 +248,7 @@ public class Group : MonoBehaviour {
 				updateGrid();
 			} else {
 				// It's not valid. revert.
-				transform.position += new Vector3(0, 1, 0);
+				transform.position += new Vector3(transform.position.x, 1, transform.position.z);
 				
 				// Clear filled horizontal lines
 				Grid.deleteFullRows();
