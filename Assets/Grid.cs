1fe57178c9916888854f573991e8d2a21b8f52ab
Felipe Luiz Tortella <felipetortella@gmail.com>
2015-12-01 20:01:25 -0200

    Implementação 4 telas
    primeira parte não testada das 4 telas

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -6,7 +6,12 @@ public class Grid : MonoBehaviour {
 	// The Grid itself
 	public static int w = 10;
 	public static int h = 20;
-	public static Transform[,] grid = new Transform[w, h];
+	public static lado = 1;
+	
+	public static Transform[,] grid1 = new Transform[w, h];
+	public static Transform[,] grid2 = new Transform[w, h];
+	public static Transform[,] grid3 = new Transform[w, h];
+	public static Transform[,] grid4 = new Transform[w, h];
 
 	public static Vector2 roundVec2(Vector2 v) {
 		return new Vector2(Mathf.Round(v.x),
@@ -21,23 +26,74 @@ public class Grid : MonoBehaviour {
 
 	public static void deleteRow(int y) {
 		for (int x = 0; x < w; ++x) {
-			Destroy(grid[x, y].gameObject);
-			grid[x, y] = null;
+			switch(lado){
+				case 1:
+					Destroy(grid1[x, y].gameObject);
+					grid1[x, y] = null;
+					break;
+				case 2:
+					Destroy(grid2[x, y].gameObject);
+					grid2[x, y] = null;
+					break;
+				case 3:
+					Destroy(grid3[x, y].gameObject);
+					grid3[x, y] = null;
+					break;
+				case 4:
+					Destroy(grid4[x, y].gameObject);
+					grid4[x, y] = null;
+					break;
+			}		
 		}
 	}
 
 	public static void decreaseRow(int y) {
 		for (int x = 0; x < w; ++x) {
-			if (grid[x, y] != null) {
-				// Move one towards bottom
-				grid[x, y-1] = grid[x, y];
-				grid[x, y] = null;
-				
-				// Update Block position
-				grid[x, y-1].position += new Vector3(0, -1, 0);
-			}
+			switch(lado){
+				case 1:
+					if (grid1[x, y] != null) {
+						// Move one towards bottom
+						grid1[x, y-1] = grid1[x, y];
+						grid1[x, y] = null;
+						
+						// Update Block position
+						grid1[x, y-1].position += new Vector3(0, -1, 0);
+					}
+					break;
+				case 2:
+					if (grid2[x, y] != null) {
+						// Move one towards bottom
+						grid2[x, y-1] = grid2[x, y];
+						grid2[x, y] = null;
+						
+						// Update Block position
+						grid[x, y-1].position += new Vector3(0, -1, 0);
+					}
+					break;
+				case 3:
+					if (grid3[x, y] != null) {
+						// Move one towards bottom
+						grid3[x, y-1] = grid3[x, y];
+						grid3[x, y] = null;
+						
+						// Update Block position
+						grid3[x, y-1].position += new Vector3(0, -1, 0);
+					}
+					break;
+				case 4:
+					if (grid3[x, y] != null) {
+						// Move one towards bottom
+						grid4[x, y-1] = grid4[x, y];
+						grid4[x, y] = null;
+						
+						// Update Block position
+						grid4[x, y-1].position += new Vector3(0, -1, 0);
+					}
+					break;				
+			}		
 		}
 	}
+					
 
 	public static void decreaseRowsAbove(int y) {
 		for (int i = y; i < h; ++i)
@@ -45,9 +101,27 @@ public class Grid : MonoBehaviour {
 	}
 
 	public static bool isRowFull(int y) {
-		for (int x = 0; x < w; ++x)
-			if (grid[x, y] == null)
-				return false;
+		for (int x = 0; x < w; ++x){
+			switch(lado){
+				case 1:	
+					if (grid1[x, y] == null)
+						return false;
+					break;
+				case 2:
+					if (grid2[x, y] == null)
+						return false;
+					break;
+				case 3:
+					if (grid3[x, y] == null)
+						return false;
+					break;
+				case 4:	
+					if (grid4[x, y] == null)
+						return false;
+					break;
+			}				
+			
+		}			
 		return true;
 	}
 
@@ -60,5 +134,4 @@ public class Grid : MonoBehaviour {
 			}
 		}
 	}
-
 }
--- Assets/Group.cs
+++ Assets/Group.cs
@@ -84,34 +84,131 @@ public class Group : MonoBehaviour {
 		}
 	}
 
-	bool isValidGridPos() {        
-		foreach (Transform child in transform) {
-			Vector2 v = Grid.roundVec2(child.position);
-			
-			// Not inside Border?
-			if (!Grid.insideBorder(v))
-				return false;
-			
-			// Block in grid cell (and not part of same group)?
-			if (Grid.grid[(int)v.x, (int)v.y] != null &&
-			    Grid.grid[(int)v.x, (int)v.y].parent != transform)
-				return false;
+	bool isValidGridPos() {
+
+		switch(Grid.lado){
+			case 1:
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+					
+					// Not inside Border?
+					if (!Grid.insideBorder(v))
+						return false;
+					
+					// Block in grid cell (and not part of same group)?
+					if (Grid.grid1[(int)v.x, (int)v.y] != null &&
+						Grid.grid1[(int)v.x, (int)v.y].parent != transform)
+						return false;
+				}
+				return true;
+			case 2:
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+					
+					// Not inside Border?
+					if (!Grid.insideBorder(v))
+						return false;
+					
+					// Block in grid cell (and not part of same group)?
+					if (Grid.grid2[(int)v.x, (int)v.y] != null &&
+						Grid.grid2[(int)v.x, (int)v.y].parent != transform)
+						return false;
+				}
+				return true;
+			case 3:
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+					
+					// Not inside Border?
+					if (!Grid.insideBorder(v))
+						return false;
+					
+					// Block in grid cell (and not part of same group)?
+					if (Grid.grid3[(int)v.x, (int)v.y] != null &&
+						Grid.grid3[(int)v.x, (int)v.y].parent != transform)
+						return false;
+				}
+				return true;
+			case 4:
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+					
+					// Not inside Border?
+					if (!Grid.insideBorder(v))
+						return false;
+					
+					// Block in grid cell (and not part of same group)?
+					if (Grid.grid4[(int)v.x, (int)v.y] != null &&
+						Grid.grid4[(int)v.x, (int)v.y].parent != transform)
+						return false;
+				}
+				return true;
 		}
-		return true;
 	}
 
 	void updateGrid() {
-		// Remove old children from grid
-		for (int y = 0; y < Grid.h; ++y)
-			for (int x = 0; x < Grid.w; ++x)
-				if (Grid.grid[x, y] != null)
-					if (Grid.grid[x, y].parent == transform)
-						Grid.grid[x, y] = null;
 		
-		// Add new children to grid
-		foreach (Transform child in transform) {
-			Vector2 v = Grid.roundVec2(child.position);
-			Grid.grid[(int)v.x, (int)v.y] = child;
-		}        
+		switch(Grid.lado){
+			
+			case 1:
+				// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid1[x, y] != null)
+							if (Grid.grid1[x, y].parent == transform)
+								Grid.grid1[x, y] = null;
+				
+				// Add new children to grid
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+					Grid.grid1[(int)v.x, (int)v.y] = child;
+				}
+				break;
+				
+			case 2:
+				// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid2[x, y] != null)
+							if (Grid.grid2[x, y].parent == transform)
+								Grid.grid2[x, y] = null;
+				
+				// Add new children to grid
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+					Grid.grid2[(int)v.x, (int)v.y] = child;
+				}
+				break;
+				
+			case 3:
+				// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid[x, y] != null)
+							if (Grid.grid3[x, y].parent == transform)
+								Grid.grid3[x, y] = null;
+				
+				// Add new children to grid
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+					Grid.grid3[(int)v.x, (int)v.y] = child;
+				}
+				break;
+				
+			case 4:
+				// Remove old children from grid
+				for (int y = 0; y < Grid.h; ++y)
+					for (int x = 0; x < Grid.w; ++x)
+						if (Grid.grid[x, y] != null)
+							if (Grid.grid4[x, y].parent == transform)
+								Grid.grid4[x, y] = null;
+				
+				// Add new children to grid
+				foreach (Transform child in transform) {
+					Vector2 v = Grid.roundVec2(child.position);
+					Grid.grid4[(int)v.x, (int)v.y] = child;
+				}
+				break;
+		}		
 	}
 }
